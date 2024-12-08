0e2e328fd00bba6c9b7d9743f19abf52a7ceb1aa
Erik Ljungdahl <ekke.ekke@gmail.com>
2024-12-08 17:56:15 +0100

    day 4 part two

--- 4.scala
+++ 4.scala
@@ -2,6 +2,7 @@
 //> using toolkit 0.6.0
 import scala.util.matching.Regex
 import scala.compiletime.ops.string
+import scala.compiletime.ops.boolean
 
 @main
 def hello(path: String): Unit =
@@ -17,6 +18,10 @@ def hello(path: String): Unit =
     .sum
 
   println(result)
+
+  val result2 = boxes(lines).filter(checkBox).length
+
+  println(result2)
 
 def variants(lines: List[List[Char]]): List[List[List[Char]]] =
   val columns = lines.transpose
@@ -35,9 +40,25 @@ def toDiagonals(lines: List[List[Char]]): List[List[Char]] =
     yield (for y <- Math
         .max(idx - (lines.length - 1), 0) to Math.min(idx, lines.length - 1)
     yield lines(y)(idx - y)))
-// val result =
-//   (for
-//     idx <- 0 until lines.length
-//     y <- 0 until idx
-//   yield lines(y)(idx - y))
   return result.toList.map(line => line.toList)
+
+def boxes(lines: List[List[Char]]): List[List[Char]] =
+  val result =
+    for
+      y <- 0 to lines.length - 3
+      x <- 0 to lines(0).length - 3
+    yield (
+      for
+        yi <- y until y + 3
+        xi <- x until x + 3
+      yield lines(yi)(xi)
+    )
+  return result.toList.map(line => line.toList)
+
+def checkBox(box: List[Char]): Boolean =
+  box match
+    case List('M', _, 'S', _, 'A', _, 'M', _, 'S') => true
+    case List('M', _, 'M', _, 'A', _, 'S', _, 'S') => true
+    case List('S', _, 'S', _, 'A', _, 'M', _, 'M') => true
+    case List('S', _, 'M', _, 'A', _, 'S', _, 'M') => true
+    case _                                         => false
